83da35814d402c7b54e75bbba06c6ff1a64ca961
André Nogueira Mateus Da Silva <2181057@my.ipleiria.pt>
2019-12-16 10:53:53 +0000

    Change camera position and background to white.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 // Criar uma cena...
 var cena =  new THREE.Scene();
-
+cena.background = new THREE.Color( 0xFFFFFF );
 // Criar uma camara
 var camara =  new THREE.PerspectiveCamera(70, 600/530, 0.1, 500);
 
@@ -8,19 +8,21 @@ var camara =  new THREE.PerspectiveCamera(70, 600/530, 0.1, 500);
 var canvas = document.getElementById('myCanvas');
 // preparar um renderer WebGL com um viewport 800x600 a adicioná-lo à pagina
 var renderer = new THREE.WebGLRenderer({ canvas: canvas });
+
 renderer.setSize(600, 530); // tamanho do canvas (da área preta)
 renderer.shadowMap.enabled = true
 
 
-camara.position.x = 4;
-camara.position.y = 3;
-camara.position.z = 2;
-camara.lookAt(0,0,0);
+camara.position.x = 0;
+camara.position.y = -5;
+camara.position.z = -15;
+
 
 
 // OrbitControls.js
 var controlos = new THREE.OrbitControls(camara, renderer.domElement);
 
+camara.lookAt(0,0,90);
 
 // GLTFLoader (BLENDER PARA THREE.JS)
 var carregador = new THREE.GLTFLoader();
@@ -32,11 +34,30 @@ carregador.load(
 );
 
 
+// Adicionar pontos de luz
+var luzPonto1 = new THREE.PointLight("white");
+luzPonto1.position.set(5, -5, -5);
+cena.add(luzPonto1);
+
 
 // Adicionar pontos de luz
 var luzPonto1 = new THREE.PointLight("white");
-luzPonto1.position.set(5, 6, 0);
-luzPonto1.castShadow = true
+luzPonto1.position.set(-5, -5, 5);
+cena.add(luzPonto1);
+
+// Adicionar pontos de luz
+var luzPonto1 = new THREE.PointLight("white");
+luzPonto1.position.set(1, 1, 1);
+cena.add(luzPonto1);
+
+// Adicionar pontos de luz
+var luzPonto1 = new THREE.PointLight("white");
+luzPonto1.position.set(5, 5, 5);
+cena.add(luzPonto1);
+
+// Adicionar pontos de luz
+var luzPonto1 = new THREE.PointLight("white");
+luzPonto1.position.set(-5, -5, -5);
 cena.add(luzPonto1);
 
 

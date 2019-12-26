1c2576c466f1e2191bd48059921e3193151ce0ae
André Silva <andre00nogueira@gmail.com>
2019-12-26 22:05:05 +0000

    Adicionar botões de interação e alguns fixes.

--- js/main.js
+++ js/main.js
@@ -2,14 +2,14 @@
 var cena =  new THREE.Scene();
 cena.background = new THREE.Color( 0xFFFFFF );
 // Criar uma camara
-var camara =  new THREE.PerspectiveCamera(70, 600/530, 0.1, 500);
+var camara =  new THREE.PerspectiveCamera(70, 530/530, 0.1, 500);
 
 
 var canvas = document.getElementById('myCanvas');
 // preparar um renderer WebGL com um viewport 800x600 a adicioná-lo à pagina
 var renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
 
-renderer.setSize(600, 530); // tamanho do canvas (da área preta)
+renderer.setSize(530, 530); // tamanho do canvas (da área preta)
 renderer.shadowMap.enabled = true
 renderer.gammaOutput= true
 renderer.setPixelRatio(window.devicePixelRatio)
@@ -17,7 +17,7 @@ renderer.setPixelRatio(window.devicePixelRatio)
 
 camara.position.x = 0;
 camara.position.y = -5;
-camara.position.z = -15;
+camara.position.z = -10;
 
 
 
@@ -47,17 +47,17 @@ luzPonto1.position.set(-5, -5, 5);
 cena.add(luzPonto1);
 
 // Adicionar pontos de luz
-var luzPonto1 = new THREE.PointLight("white");
+luzPonto1 = new THREE.PointLight("white");
 luzPonto1.position.set(1, 1, 1);
 cena.add(luzPonto1);
 
 // Adicionar pontos de luz
-var luzPonto1 = new THREE.PointLight("white");
+luzPonto1 = new THREE.PointLight("white");
 luzPonto1.position.set(5, 5, 5);
 cena.add(luzPonto1);
 
 // Adicionar pontos de luz
-var luzPonto1 = new THREE.PointLight("white");
+luzPonto1 = new THREE.PointLight("white");
 luzPonto1.position.set(-5, -5, -5);
 cena.add(luzPonto1);
 
